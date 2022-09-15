d6fdd67bd3166ab2822105373ea74c30cab812b8
arcjjang <arcelias@naver.com>
2022-09-16 08:57:09 +0900

    실전! 스프링 데이터 JPA - 확장 기능 : Auditing - 스프링 데이터 JPA 사용

--- src/main/java/study/datajpa/StudySpringDataJpaApplication.java
+++ src/main/java/study/datajpa/StudySpringDataJpaApplication.java
@@ -2,7 +2,14 @@ package study.datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.Optional;
+import java.util.UUID;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class StudySpringDataJpaApplication {
 
@@ -10,4 +17,9 @@ public class StudySpringDataJpaApplication {
         SpringApplication.run(StudySpringDataJpaApplication.class, args);
     }
 
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        // 실제론 session 에서 꺼내서 쓰면 된다.
+        return () -> Optional.of(UUID.randomUUID().toString());
+    }
 }
